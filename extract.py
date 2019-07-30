6b2342580ea05222a271d314f5964b3ea1fbbea8
Duong Nguyen-Huu <5943271+duongnhn@users.noreply.github.com>
2019-07-30 12:57:43 -0700

    add code_section_offset

--- extract.py
+++ extract.py
@@ -31,6 +31,8 @@ def read_var_uint(wasm, pos):
 
 def strip_debug_sections(wasm):
   logging.debug('Strip debug sections')
+  print('Strip debug sections')
+  code_section_offset = 0
   pos = 8
   stripped = wasm[:pos]
   while pos < len(wasm):
@@ -42,13 +44,18 @@ def strip_debug_sections(wasm):
       name_len, name_pos = read_var_uint(wasm, section_body)
       name_end = name_pos + name_len
       name = wasm[name_pos:name_end]
+      #print(name)
       if name == "linking" or name == "sourceMappingURL" or name.startswith("reloc..debug_") or name.startswith(".debug_"):
+        print("skip section ", name)
         continue  # skip debug related sections
+    elif section_id == 10: # Code section
+      code_section_offset = len(stripped)
     stripped = stripped + wasm[section_start:pos]
-  return stripped
+  return (stripped, code_section_offset)
 
 def strip_wasm_sections(wasm):
   logging.debug('Strip wasm sections')
+  print('Strip wasm sections')
   pos = 8
   stripped = wasm[:pos]
   while pos < len(wasm):
@@ -60,7 +67,10 @@ def strip_wasm_sections(wasm):
       name_len, name_pos = read_var_uint(wasm, section_body)
       name_end = name_pos + name_len
       name = wasm[name_pos:name_end]
-      if name == "linking" or name == "sourceMappingURL" or name.startswith("reloc..debug_") or name.startswith(".debug_"):
+      #print(name)
+      #if name == "linking" or name == "sourceMappingURL" or name.startswith("reloc..debug_") or name.startswith(".debug_"):
+      if name == "linking" or name.startswith("reloc..debug_") or name.startswith(".debug_"):
+        print("adding section ", name)
         stripped = stripped + wasm[section_start:pos]
         # skip wasm related sections
   return stripped
@@ -75,10 +85,15 @@ def encode_uint_var(n):
 
 def append_debug_mapping(wasm, url):
   logging.debug('Append debugSymbolsURL section')
-  section_name = "debugSymbolsURL"
+  section_name = "sourceMappingURL"
   section_content = encode_uint_var(len(section_name)) + section_name + encode_uint_var(len(url)) + url
   return wasm + encode_uint_var(0) + encode_uint_var(len(section_content)) + section_content
 
+def append_code_section_offset(wasm, code_section_offset):
+  logging.debug('Append codeSectionOffset section')
+  section_name = "codeSectionOffset"
+  section_content = encode_uint_var(len(section_name)) + section_name + encode_uint_var(code_section_offset)
+  return wasm + encode_uint_var(0) + encode_uint_var(len(section_content)) + section_content
 
 def main():
   # example: python extract.py fib.wasm --dwarf fib.dwarf --wasm out.wasm
@@ -86,15 +101,15 @@ def main():
   options = parse_args()
   with open(options.input, 'rb') as infile:
     wasm_input = infile.read()
+  with open(options.wasm, 'wb') as outfile_wasm:
+    wasm, code_section_offset = strip_debug_sections(wasm_input)
+    #print("code_section_offset: ", code_section_offset)
+    outfile_wasm.write(append_debug_mapping(wasm, "http://localhost:8889/" + options.dwarf))
   with open(options.dwarf, 'wb') as outfile_dwarf:
     dwarf = strip_wasm_sections(wasm_input)
-    outfile_dwarf.write(dwarf)
-  with open(options.wasm, 'wb') as outfile_wasm:
-    wasm = strip_debug_sections(wasm_input)
-    outfile_wasm.write(append_debug_mapping(wasm, "http://localhost:8000/"+options.dwarf))
+    outfile_dwarf.write(append_code_section_offset(dwarf, code_section_offset))
   logging.debug('Done')
   return 0
 if __name__ == '__main__':
   logging.basicConfig(level=logging.DEBUG if os.environ.get('EMCC_DEBUG') else logging.INFO)
   sys.exit(main())
-
